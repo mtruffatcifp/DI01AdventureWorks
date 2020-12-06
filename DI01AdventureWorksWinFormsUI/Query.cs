b0cf88a30a5bb261bd88c475f6b084b0d288fd8e
Marco Truffat <marcotruffat@paucasesnovescifp.cat>
2020-12-06 02:02:54 +0100

    Added Query class to manage queries  more easily.
    The class has 8 atributtes for the corresponding filters of the UI. Also has method to check if the attribute is NULL or not.

---
+++ DI01AdventureWorksWinFormsUI/Query.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI01AdventureWorksWinFormsUI
+{
+    public class Query
+    {
+        public string Language { get; set; }
+        public string Category { get; set; }
+        public string Subcategory { get; set; }
+        public string  Size { get; set; }
+        public string Class { get; set; }
+        public string Style { get; set; }
+        public string ProductLine { get; set; }
+        public bool Availability { get; set; }
+
+        public Query(string language, string category, string subcateogry, string size, string clas, string style, string productLine, bool availability)
+        {
+            Language = checkNull(language);
+            Category = checkNull(category);
+            Subcategory = checkNull(subcateogry);
+            Size = checkNull(size);
+            Class = checkNull(clas);
+            Style = checkNull(style);
+            ProductLine = checkNull(productLine);
+            Availability = availability;
+        }
+        private string checkNull(string tocheck)
+        {
+            if (tocheck == null)
+            {
+                return "NULL";
+            } else
+            {
+                return $"'{tocheck}'";
+            }
+                    
+        }
+
+        public string ToQuery()
+        {
+            return $"EXEC spGeneric_ShowResult @Idioma = {Language}, @Category = {Category}, @Subcategory = {Subcategory}, " +
+                    $"@Size = {Size}, @Class = {Class}, @Style = {Style}, @ProductLine = {ProductLine}, @Availability = {Availability};";
+        }
+    }
+}
